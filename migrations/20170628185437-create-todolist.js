4b02b981f9e977a2b25e99237e64a6b733359e78
Robert Abell <robertabell@Roberts-MacBook-Pro.local>
2017-06-29 13:03:44 -0500

    update

---
+++ migrations/20170628185437-create-todolist.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('todolists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      completed: {
+      type:Sequelize.BOOLEAN,
+      allowNull: false, 
+      defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('todolists');
+  }
+};
